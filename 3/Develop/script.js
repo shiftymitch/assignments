493a3c52f0a34f868ee7b06f22be7509e001c39f
shiftymitch <shiftymitch@gmail.com>
2020-03-10 09:33:26 -0600

    pushed wrong files, re-pushed

--- 3/Develop/script.js
+++ 3/Develop/script.js
@@ -1,6 +1,43 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword (length) {
+
+  length = prompt("How long should the password be?");
+  var result = "";
+  var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerChar = "abcdefghijklmnopqrstuvwxyz";
+  var numChar = "0123456789";
+  var specialChar = "!@#$%^&*"
+
+  
+  //my first method
+
+  // for (var i = 0; i < length; i++) {
+  //   result += upperChar.charAt(Math.floor(Math.random() * upperChar.length));
+  //   result += lowerChar.charAt(Math.floor(Math.random() * lowerChar.length));
+  //   result += numChar.charAt(Math.floor(Math.random() * numChar.length));
+  //   result += specialChar.charAt(Math.floor(Math.random() * specialChar.length));
+  // }
+
+
+  //my 2nd method
+
+  for (var i = 0; i < length; i++) {
+    var temp = [
+      upperChar.charAt(Math.floor(Math.random() * upperChar.length)), 
+      lowerChar.charAt(Math.floor(Math.random() * lowerChar.length)), 
+      numChar.charAt(Math.floor(Math.random() * numChar.length)), 
+      specialChar.charAt(Math.floor(Math.random() * specialChar.length))];
+    
+    result += temp[Math.floor(Math.random() * 4)];
+  }
+
+  return result;
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
