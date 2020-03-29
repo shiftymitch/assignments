12a86782fb5e05c010ed3c4a0b9e23d98f11c309
shiftymitch <shiftymitch@gmail.com>
2020-03-29 10:25:53 -0600

    final solutions

--- 5/Assets/js/scripts.js
+++ 5/Assets/js/scripts.js
@@ -1,37 +1,43 @@
-//global variables
 var today = moment();
 
 $(document).ready(function() {
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
+
 $("#currentDay").text(today.format("dddd, MMMM Mo, YYYY")).css("font-weight", "bold");
 
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-
-//create time blocks / second attempt
+//create time blocks 
 for (var i = 1; i < 10; i++) {
     var newRow = $("<div>").addClass("row time-block block"+i);
     var hourCol = $("<div>").addClass("col-3 hour hour"+i);
     var tasks = $("<div>").addClass("col-6 tasks task"+i);
     var button = $("<div>").addClass("col-3 btn btn-secondary saveBtn button"+i);
     var hour = moment().startOf("day").add(7+i, "h");
-    var input = $("<input>").width("100%").height("100%");
+    var input = $("<textarea>").width("100%").height("100%");
+    var hH = parseInt(hour.format("H"));
+    var tH = parseInt(today.format("H"));
 
     $(".container").append(newRow);
     newRow.append(hourCol, tasks, button);
     $(".hour"+i).text(hour.format("h:mmA"));
+
+    //color coded time blocks
+    if (hH < tH) { 
+        newRow.addClass("past");
+    } else if (hH > tH) { 
+        newRow.addClass("future");
+    } else if (hH = tH) { 
+        newRow.addClass("present");
+    }
+
     $(".task"+i).append(input);
     $(".button"+i).text("SAVE");
     $(".button"+i).attr("id", i);
     
+    //save tasks to localStorage
     var tasks = [];
     var storedTask = localStorage.getItem("task "+i+"");
     tasks.push(storedTask);
     $(".task"+i)[0].firstChild.value = tasks;
 }
-
-
 
 //save task input
 $(".saveBtn").on("click", function() {
@@ -53,52 +59,5 @@ $(".tasks").on("click", function(event) {
         inputVal.readOnly = !editable;
     }
 })
-
-// $(this).on("click", function(event) {
-//     console.log(event.target);
-// })
-
-
-//create time blocks / first attempt
-// for (var i = 1; i < 11; i++) {
-//     //time block variables
-//     var clockIn = moment().startOf("day").add(8, "h");
-//     var newBlock = $("<div>");
-//     var time = $("<h5>");
-//     var task = $("<input>").width("100%").height("70%");
-//     var nextTime = moment(clockIn).add(i-1, "h").format("h:mmA");
-//     //append new time blocks & format them
-//     $(".container").append(newBlock);
-//     newBlock.addClass("block"+i);
-//     newBlock.height("120px");
-//     newBlock.css("border", "2px solid black");
-//     $(".block"+i).append(time);
-//     time.text(nextTime);
-//     time.css("margin", "5px");
-//     $(".block"+i).append(task);
-//     //change opacity of time blocks in past
-//     if (parseInt(nextTime.charAt(0) + nextTime.charAt(1)) < today.format("h") && nextTime.charAt(nextTime.length-2) !== "P") {
-//         newBlock.css("opacity", "20%");
-//     }
-// }
-
-
-
-
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-
-
-
-// WHEN I click into a timeblock
-// THEN I can enter an event
-
-
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-
-
-// WHEN I refresh the page
-// THEN the saved events persist
 
 });
